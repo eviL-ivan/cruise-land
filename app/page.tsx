b5507732aab0a65fd7ec020bbf1f4b29a26c9fa7
SomeoneAtLeast <vadim.babichev@wonderfamily.com>
2025-10-08 20:06:42 +0200

    no CTA

--- app/page.tsx
+++ app/page.tsx
@@ -48,9 +48,9 @@ export default function Home() {
       <main className="min-h-screen">
         <Hero />
         <Overview />
-        {/*<div id="map">*/}
-        {/*  {preloadMap && <AntarcticaMap />}*/}
-        {/*</div>*/}
+        <div id="map">
+          {preloadMap && <AntarcticaMap />}
+        </div>
         <div id="journey">
           <Journey />
         </div>
@@ -64,9 +64,9 @@ export default function Home() {
         {/*<div id="cabins">*/}
         {/*  <Cabins />*/}
         {/*</div>*/}
-        {/*<div id="contact">*/}
-        {/*  <CTA />*/}
-        {/*</div>*/}
+        <div id="contact">
+          <CTA />
+        </div>
         <Footer />
       </main>
     </>
