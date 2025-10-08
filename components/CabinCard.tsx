89dfac4206bc883c3cb8b1fbe4d738695392dc35
SomeoneAtLeast <vadim.babichev@wonderfamily.com>
2025-10-08 20:43:30 +0200

    Videos

--- components/CabinCard.tsx
+++ components/CabinCard.tsx
@@ -29,7 +29,7 @@ export function CabinCard({ cabin, onBook, selectButtonText, index }: CabinCardP
   const [selectedMediaIndex, setSelectedMediaIndex] = useState(0) // Selected media for gallery
   const cabinImages = cabin.images || [cabin.image || '/placeholder.svg']
   const cabinVideos = cabin.videos || []
-  const allMedia = [...cabinImages, ...cabinVideos] // Combine images and videos
+  const allMedia = [...cabinImages ] // Combine images and videos
   const isEven = index % 2 === 0
 
   const {
@@ -260,15 +260,15 @@ export function CabinCard({ cabin, onBook, selectButtonText, index }: CabinCardP
       </div>
 
       {/* Fullscreen Media Gallery */}
-      {/*<MediaGalleryDialog*/}
-      {/*  isOpen={isGalleryOpen}*/}
-      {/*  onClose={() => setIsGalleryOpen(false)}*/}
-      {/*  media={allMedia}*/}
-      {/*  initialIndex={selectedMediaIndex}*/}
-      {/*  cabinName={cabin.name}*/}
-      {/*  imageCount={cabinImages.length}*/}
-      {/*  onIndexChange={handleGalleryIndexChange}*/}
-      {/*/>*/}
+      <MediaGalleryDialog
+        isOpen={isGalleryOpen}
+        onClose={() => setIsGalleryOpen(false)}
+        media={allMedia}
+        initialIndex={selectedMediaIndex}
+        cabinName={cabin.name}
+        imageCount={cabinImages.length}
+        onIndexChange={handleGalleryIndexChange}
+      />
     </div>
   )
 }
